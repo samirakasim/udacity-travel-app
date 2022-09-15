9be79332a1e8ea9d366f0615b4fc85a940d05a61
Samira Adam <samiraadam17_@hotmail.com>
2022-09-15 13:55:22 +0100

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,33 @@
+var path = require('path')
+const express = require('express')
+// const mockAPIResponse = require('./mockAPI.js')
+
+const app = express()
+
+app.use(express.static('dist'))
+
+/* Dependencies */
+const bodyParser = require('body-parser');
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors =  require('cors');
+app.use(cors());
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8000, function () {
+    console.log('Example app listening on port 8000!')
+})
+
+// app.get('/test', function (req, res) {
+//     res.send(mockAPIResponse)
+// })
